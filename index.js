2fc701135c8bd630223b0c0c0fa98e5aaa97500f
TahidulAlam <tahid.cse@gmail.com>
2023-10-20 03:37:10 +0600

    In this commit , i add product add,update,delete functreq

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// tahidcse
-// fhv6jjhoPYpURnxu
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
@@ -25,30 +23,159 @@ async function run() {
   try {
     await client.connect();
     const slider_image = client.db("brand").collection("slider_image");
-    const brand_details = client.db("brand").collection("brand details");
-    const addproducts = client.db("brand").collection("addproducts");
+    const GariDekhunDbNew = client
+      .db("GariDekhunDbNew")
+      .collection("garidekhunCollectionNew");
+    const allproducts = client
+      .db("GariDekhunDbNew")
+      .collection("garikinunProducts");
+    const cartProducts = client
+      .db("GariDekhunDbNew")
+      .collection("garikinunCarts");
+
     app.get("/home/slider", async (req, res) => {
-      const cursur = slider_image.find();
-      const result = await cursur.toArray();
-      res.send(result);
+      try {
+        const cursor = slider_image.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
     });
+
     app.get("/home", async (req, res) => {
-      const cursur = brand_details.find();
-      const result = await cursur.toArray();
-      res.send(result);
+      try {
+        const cursor = GariDekhunDbNew.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
     });
+
     app.get("/home/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {
-        _id: new ObjectId(id),
-      };
-      const result = await brand_details.findOne(query);
+      try {
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).send("Invalid ID format");
+        }
+        const query = { _id: new ObjectId(id) };
+        const result = await GariDekhunDbNew.findOne(query);
+        if (!result) {
+          return res.status(404).send("Product not found");
+        }
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    app.get("/allproducts", async (req, res) => {
+      try {
+        const cursor = allproducts.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    app.get("/allproducts/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allproducts.findOne(query);
       res.send(result);
     });
-    app.post("/addproducts", async (req, res) => {
-      const product = req.body;
-      const result = await addproducts.insertOne(product);
-      console.log(result);
+    app.get("/allproducts/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allproducts.findOne(query);
+      res.send(result);
+    });
+    app.put("/allproducts/update/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const UpdateContent = req.body;
+        console.log(UpdateContent);
+        const filter = { _id: new ObjectId(id) };
+        const option = { upsert: true };
+        const updatedProduct = {
+          $set: {
+            brand: UpdateContent.brand,
+            name: UpdateContent.name,
+            image_link: UpdateContent.image_link,
+            car_type: UpdateContent.car_type,
+            price: UpdateContent.price,
+            ratings: UpdateContent.ratings,
+            description: UpdateContent.description,
+          },
+        };
+        const result = await allproducts.updateOne(
+          filter,
+          updatedProduct,
+          option
+        );
+        if (result.modifiedCount > 0) {
+          res.status(200).json({ message: "Product updated successfully" });
+        } else {
+          res.status(404).json({ message: "Product not found" });
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
+
+    app.get("/allproducts/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      try {
+        const cursor = allproducts.find({ brand: brand });
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    app.post("/allproducts", async (req, res) => {
+      try {
+        const productData = req.body;
+        const result = await allproducts.insertOne(productData);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+    app.post("/cart", async (req, res) => {
+      try {
+        const addCart = req.body;
+        const result = await cartProducts.insertOne(addCart);
+        res.status(201).json(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
+    app.get("/cart", async (req, res) => {
+      try {
+        const cursor = cartProducts.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartProducts.deleteOne(query);
       res.send(result);
     });
     await client.db("admin").command({ ping: 1 });
@@ -56,9 +183,11 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
+    // Leave the client open in a server application.
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
